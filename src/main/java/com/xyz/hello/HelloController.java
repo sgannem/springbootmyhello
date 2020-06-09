7130c302b7c1ea0613d386772faab8de2e40e3eb
Srinivasu Gannem <srinivasu.gannem@nxp.com>
2020-06-09 21:35:01 +0530

    Added swagger

--- src/main/java/com/xyz/hello/HelloController.java
+++ src/main/java/com/xyz/hello/HelloController.java
@@ -2,11 +2,14 @@ package com.xyz.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/hello/hi")
+    @ApiOperation(value = "/hello/hi", notes = "it greets.", response = String.class)
     public String index() {
         return "Greetings from Spring Boot!";
     }
